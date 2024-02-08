69be2a1d27bf26c73cf0aafcedfee44e9f93cb57
F-Kibanov <c-280d@mail.ru>
2024-02-09 00:56:46 +0500

    mvp v0.0.4

--- recipes/forms.py
+++ recipes/forms.py
@@ -1,11 +1,5 @@
-import datetime
-
 from django import forms
-from django.db.models import DO_NOTHING
-from django.forms import TimeInput, RadioSelect
-from django.forms.widgets import ChoiceWidget, CheckboxSelectMultiple
-
-from .models import Recipe
+from django.forms import TimeInput
 
 
 class AddRecipeForm(forms.Form):
@@ -27,37 +21,30 @@ class AddRecipeForm(forms.Form):
                                                             'Укроп 1шт.'}))
     instructions = forms.CharField(label='Способ приготовления',
                                    widget=forms.Textarea(attrs={'class': 'form-control', 'placeholder':
-'''1. Отварить картофель "в мундире", морковь и яйца. Остудить. Почистить.
-2. Все подготовленные составляющие (кроме лука и рыбы) измельчить на средней по размеру ячейке тёрки.
-3. Луковицу мелко порубить, залить на 5 минут кипятком, замариновать.
-4. Первым слоем выложить половину объёма тертого вареного картофеля. Смазать или сделать сетку из качественного, жирного майонеза.
-5. На каждом последующем слое, кроме верхнего, так же сделать сетку или смазывать майонезом.
-6. Второй слой – предварительно размятая рыба.
-7. Следом мелкорубленный промаринованный лук.
-8. Оставшаяся половинка натёртого картофеля.
-9. Затем перетёртая морковь.
-10. Слой тёртого сыра.
-11. Пласт яичного белка.
-12. В финале покрыть протёртым прямо на блюдо желтком.'''
+                                       '''1. Отварить картофель "в мундире", морковь и яйца. Остудить. Почистить.
+                                       2. Все подготовленные составляющие (кроме лука и рыбы) измельчить на средней по размеру ячейке тёрки.
+                                       3. Луковицу мелко порубить, залить на 5 минут кипятком, замариновать.
+                                       4. Первым слоем выложить половину объёма тертого вареного картофеля. Смазать или сделать сетку из качественного, жирного майонеза.
+                                       5. На каждом последующем слое, кроме верхнего, так же сделать сетку или смазывать майонезом.
+                                       6. Второй слой – предварительно размятая рыба.
+                                       7. Следом мелкорубленный промаринованный лук.
+                                       8. Оставшаяся половинка натёртого картофеля.
+                                       9. Затем перетёртая морковь.
+                                       10. Слой тёртого сыра.
+                                       11. Пласт яичного белка.
+                                       12. В финале покрыть протёртым прямо на блюдо желтком.'''
                                                                 }))
     time_to_cook = forms.TimeField(label='Время приготовления (по желанию)', required=False,
                                    widget=TimeInput(attrs={'class': 'form-control'}))
-    # image = forms.ImageField(label='Изображение готового блюда', required=False)
     author = forms.CharField(label='Автор рецепта', required=False,
                              widget=forms.Textarea(attrs={'class': 'form-control',
                                                           'placeholder': 'Неизвестен'}))
-
-    # add_date = forms.DateTimeField(initial=datetime.datetime.now)
 
     def __str__(self):
         return self.title
 
 
 class AddUserForm(forms.Form):
-    # choices = []
-    # recipes = Recipe.objects.all()
-    # for recipe in recipes:
-    #     choices.append(tuple([str(recipe.title[:1]), str(recipe.title)]))
     name = forms.CharField(label='Имя', max_length=100,
                            widget=forms.TextInput(attrs={'class': 'form-control',
                                                          'placeholder': 'Иван'}))
@@ -69,9 +56,6 @@ class AddUserForm(forms.Form):
                                                             'placeholder': 'user@example.com'}))
     phone = forms.CharField(label='Телефон', max_length=20,
                             widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': '+7-999-000-00-00'}))
-    # recipes = forms.ChoiceField(label='Ваши рецепты', widget=CheckboxSelectMultiple, choices=choices, required=False)
-
-    # register_date = forms.DateField(initial=datetime.date.today)
 
     def __str__(self):
         return f'{self.name} {self.surname}'
